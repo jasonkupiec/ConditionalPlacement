fdcbfe9cc00bbc8d332d50f1e5078c9c78cd1e72
rosewebdev <jason@rosewebdev.com>
2014-12-09 09:54:16 -0700

    Initial Commit

---
+++ ConditionalPlacement/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ConditionalPlacement
+//
+//  Created by Jason Kupiec on 12/9/14.
+//  Copyright (c) 2014 Jason Kupiec. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ConditionalPlacement/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ConditionalPlacement
+//
+//  Created by Jason Kupiec on 12/9/14.
+//  Copyright (c) 2014 Jason Kupiec. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
